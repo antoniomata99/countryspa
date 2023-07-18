4cb7bd32573f717c6da844cf2fa6c4bcdb10fd6c
Antonio Mata <antoniomata99@hotmail.com>
2023-07-17 21:15:01 -0500

    SearchBox Component created

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CountriesRoutingModule } from './countries-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
@@ -19,7 +21,8 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
   ],
   imports: [
     CommonModule,
-    CountriesRoutingModule
+    CountriesRoutingModule,
+    SharedModule
   ]
 })
 export class CountriesModule { }
