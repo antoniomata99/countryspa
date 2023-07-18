becda7747a29a07e93048daf285f6099bf783480
Antonio Mata <antoniomata99@hotmail.com>
2023-07-17 22:15:55 -0500

    SearchBox event added

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -9,4 +9,10 @@ import { Component, Input } from '@angular/core';
 export class SearchBoxComponent {
   @Input()
   public placeholder: string = '';
+
+  @Output() onValue = new EventEmitter<string>();
+
+  emitValue(txtSearch:string):void{
+    this.onValue.emit(txtSearch);
+  }
 }
