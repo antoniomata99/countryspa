1f8793ba82af9476b60f22d6dbd103eac68f5db2
Antonio Mata <antoniomata99@hotmail.com>
2023-07-28 12:56:04 -0500

    New items added

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,13 +7,34 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styles: [
   ]
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit{
+
+  private debouncer: Subject<string> = new Subject<string> //Tipo especial de Observable
+
   @Input()
   public placeholder: string = '';
 
-  @Output() onValue = new EventEmitter<string>();
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value => {
+      this.onDebounce.emit( value );
+    });
+  }
 
   emitValue(txtSearch:string):void{
     this.onValue.emit(txtSearch);
+  }
+
+  onKeyPress(searchTerm: string){
+    this.debouncer.next( searchTerm );
   }
 }
